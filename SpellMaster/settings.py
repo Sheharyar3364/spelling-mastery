d626ae892d4dc44bf80c23c1b9c8380c1c12eef0
Muhammad Sheharyar <muhammadsheharyar3364@gmail.com>
2024-08-07 16:45:52 +0500

    changed debug mode

--- SpellMaster/settings.py
+++ SpellMaster/settings.py
@@ -30,7 +30,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
