b56226edf1d89ec7ef88200fb170acb64bc48791
Muhammad Sheharyar <muhammadsheharyar3364@gmail.com>
2024-08-08 12:27:07 +0500

    added build file

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Install dependencies from requirements.txt
 pip install -r requirements.txt
-python3 manage.py migrate
+
+# Run custom management command to download NLTK data
 python3 manage.py download_nltk_data
+
+# Collect static files
+python3 manage.py collectstatic --noinput
+
+# Apply database migrations
+python3 manage.py migrate
